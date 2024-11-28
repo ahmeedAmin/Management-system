d5d6dcf8eccd10e46d701ceb8daaf0c4cc0ecabf
ahmed amin <66143802+ahmeedAmin@users.noreply.github.com>
2024-11-28 13:52:49 +0300

    Add files via upload

---
+++ EmployeeSystemMangement.DAL/Data/ApplicationDBContext.cs
@@ -0,0 +1,32 @@
+﻿using EmployeeSystemMangement.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSystemMangement.DAL.Data
+{
+	public class ApplicationDBContext : IdentityDbContext<ApplicationUsers, ApplicationRoles, string>
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+          
+
+        }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+
+    }
+}
---
+++ EmployeeSystemMangement.DAL/Entities/Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSystemMangement.DAL.Entities
+{
+     public class Department:BaseEntity
+    {
+        [Required(ErrorMessage ="Code is Required.")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "Name is Required.")]
+        public string Name { get; set; }
+        [Display(Name = "Date Of Creation") ]
+        [Required(ErrorMessage = "Date is Required.")]
+
+        public DateTime? DateOfCreation { get; set; }
+        [InverseProperty("Department")]
+        public ICollection<Employee> Employees { get; set; } = new HashSet<Employee>();
+    }
+}
