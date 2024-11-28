c12aef90f4a8d9a90216e43860a8450df48377bb
ahmed amin <66143802+ahmeedAmin@users.noreply.github.com>
2024-11-28 14:00:51 +0300

    Add files via upload

---
+++ EmployeeSystemMangement.PL/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeSystemMangement.BLL.Interfaces;
+using EmployeeSystemMangement.BLL.Repositories;
+using EmployeeSystemMangement.PL.Services.SendEmail;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmployeeSystemMangement.PL.Extensions
+{
+    public static class ApplicationServicesExtensions
+    {
+        public static void AddApplicationServicesExtensions(this IServiceCollection services)
+        {
+            //services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            //services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddTransient<ISendEmail, SendEmail>();
+        }
+    }
+}
